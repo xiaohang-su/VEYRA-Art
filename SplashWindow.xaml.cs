840ce8f21083e735543e8bd5996eec7c5639427c
xiaohang-su <xiaohang114514@gmail.com>
2026-09-19 04:43:11 +0800

    fix: ?????? - ??System.Windows.Controls?Media.Effects??

--- SplashWindow.xaml.cs
+++ SplashWindow.xaml.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using System.Windows.Shapes;
 
 namespace ArtTest
